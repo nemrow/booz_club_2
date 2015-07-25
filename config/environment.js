a3ce1c474e943876e60f8e0d71751f6d8b274682
Nemrow <nemrowj@gmail.com>
2015-07-25 13:24:12 -0700

    Add ember fire

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'booz-club-2',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
+    firebase: 'https://booz-club.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
